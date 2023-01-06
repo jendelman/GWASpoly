57a9cfeee655927e762ddfc62167f12c54f44526
jendelman <endelman@wisc.edu>
2023-01-06 08:57:02 -0600

    v2.12

--- R/VCF2dosage.R
+++ R/VCF2dosage.R
@@ -1,12 +1,12 @@
-#' Prepare GWASpoly dosage input file from VCF file
+#' Convert VCF to dosage file
 #' 
-#' Prepare GWASpoly dosage input file from VCF file
+#' Convert VCF to dosage file
 #' 
-#' The "GT" option for \code{geno.code} is the posterior maximum genotype (e.g., 0/0/1/1), while "DS" represents the posterior mean dosage of the alternate allele. (At present, only bi-allelic variants are supported.) The VCF file must conform to version 4.1 of the VCF specification or later. 
+#' Only bi-allelic variants supported. The "GT" option for \code{geno.code} is the posterior maximum genotype (e.g., 0/0/1/1). "DS" represents the posterior mean dosage of the alternate allele. VCF file must conform to 4.1 or later.  
 #'
-#' @param VCF.file VCF filename to read in
-#' @param dosage.file name of CSV file to output with allele dosage
-#' @param geno.code genotype code in the FORMAT field: either "GT" or "DS" (see Details)
+#' @param VCF.file VCF filename (can be gzipped)
+#' @param dosage.file CSV filename to output with allele dosage
+#' @param geno.code genotype code in the FORMAT field: "GT" of "DS"
 #' @param ploidy ploidy
 #' @param samples optional vector of sample names, to export subset of the population
 #' @param min.DP minimum per sample depth (DP) to export genotype. Default is 1, for no filtering.
@@ -67,6 +67,7 @@ VCF2dosage <- function(VCF.file, dosage.file, geno.code, ploidy, samples=NULL,
   }
   
   m1 <- m <- 0
+  n.minor <- 0
   while(length(temp) > 0) {
     m1 <- m1 + 1
     if (m1%%1e4==0) {cat(sub("X",m1,"Number of variants processed...X\n"))}
@@ -94,17 +95,20 @@ VCF2dosage <- function(VCF.file, dosage.file, geno.code, ploidy, samples=NULL,
 		  } 
   
 		  data <- strsplit(temp2[-(1:9)],split=":",fixed=T)[sample.ix]  #holds string of data for each sample, as a list
-		  if (geno.code=="DS")
-		    geno <- sapply(data,function(w){as.numeric(w[geno.pos])})
-		  if (geno.code=="GT") 
-		    geno <- sapply(data,function(w){
-		      if(length(w) < geno.pos || w[geno.pos]==".") {
-		        return(as.numeric(NA))  
-		      } else {
-		        y <- strsplit(w[geno.pos],split="/",fixed=T)[[1]]
-		        return(sum(as.integer(y)))
-		      }
-		    })
+		  
+		  convert <- function(w,geno.code,geno.pos) {
+		    if(length(w) < geno.pos || w[geno.pos]==".")
+		      return(as.numeric(NA))  
+		    if (geno.code=="GT") {
+		      z <- as.integer(strsplit(w[geno.pos],split="/",fixed=T)[[1]])
+		      return(sum(z))
+		    }
+		    if (geno.code=="DS"){
+		      return(as.numeric(w[geno.pos]))
+		    }
+		  }
+		  geno <- sapply(data,convert,geno.code=geno.code,geno.pos=geno.pos)
+		  
 		  if (min.DP > 1) {
 		    DPvec <- sapply(data,function(w){
 		      if(length(w) < DP.pos || w[DP.pos]==".") {
@@ -127,6 +131,7 @@ VCF2dosage <- function(VCF.file, dosage.file, geno.code, ploidy, samples=NULL,
 		    }
 		  }
 		  
+		  geno <- round(geno,1)
 		  if (frac.miss <= max.missing & n.minor >= min.minor) {
   		  writeLines(con=out1,text=paste(c(marker,temp2[1],temp2[2],REF,ALT,geno),collapse=delim))
   		  m <- m + 1
